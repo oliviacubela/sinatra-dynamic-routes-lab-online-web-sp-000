04b4aeeac13282399655a427fba841b311c1efe4
Olivia Cubela <oliviacubela@gmail.com>
2020-09-21 19:22:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
     @word5 = params[:word5]
     @word1 + " " + @word2 + " " + @word3 + " " + @word4 + " " + @word5 + "."
 
-    # string.join(" ")keeps giving error
+    # string.join(" ")keeps giving error - revisit
   end
 
   get '/:operation/:number1/:number2' do
