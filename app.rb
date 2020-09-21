1d9978dcbaeec760044184914f8028ca161bbdf4
Olivia Cubela <oliviacubela@gmail.com>
2020-09-21 17:47:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    "@number.times" do
+    @number.to_s.times do
       puts "#{@phrase}"
     end
     #repeats the phrase n times
