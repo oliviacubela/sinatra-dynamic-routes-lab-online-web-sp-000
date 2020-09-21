697539bff4d62fad3b9259af606501965aa8fade
Olivia Cubela <oliviacubela@gmail.com>
2020-09-21 18:30:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,12 +17,12 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    params[:number].times do 
+    params[:number].times do
       params[:phrase]
     end
     params[:phrase]
   end
-    #repeats the phrase n times    
+    #repeats the phrase n times
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     #concatenates the words and adds a period
