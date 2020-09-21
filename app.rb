1752df80f6cea2c4631e94a587523c1d515759d8
Olivia Cubela <oliviacubela@gmail.com>
2020-09-21 17:25:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,28 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    #renders the name backwards
+  end
+
+  get '/square/:number' do
+    #renders the square of the number
+  end
+
+  get '/say/:number/:phrase' do
+    #repeats the phrase n times
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    #concatenates the words and adds a period
+  end
+
+  get '/:operation/:number1/:number2' do
+    #adds two numbers together
+    #subtracts the second number from the first
+    #multiplies two numbers together
+    #divides the first number by the second number
+  end
 
 end
