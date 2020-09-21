d67234ef5d30b68b9ccc8b07a88f233ff1e20188
Olivia Cubela <oliviacubela@gmail.com>
2020-09-21 19:16:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -37,7 +37,13 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
+
+    case @operation 
+    when "add"
+      @number1 + @number2
+    end
     #adds two numbers together
+  when "subtracts"
     #subtracts the second number from the first
     #multiplies two numbers together
     #divides the first number by the second number
