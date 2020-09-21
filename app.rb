052a80402910b3b816003b8bf13c4a8f2a1a4f49
Olivia Cubela <oliviacubela@gmail.com>
2020-09-21 19:13:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
-    @word5 + " " + @word5 + " " + @word5 + " " + @word5 + " " + @word5 + "."
+    @word1 + " " + @word2 + " " + @word3 + " " + @word4 + " " + @word5 + "."
 
     # string.join(" ")keeps giving error #concatenates the words and adds a period
   end
